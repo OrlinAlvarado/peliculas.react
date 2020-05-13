9f717ebd55d4e664a2fd3aeb011027d6bb1315a4
Orlin Alvarado <oalvaradomorales@gmail.com>
2020-05-12 21:32:05 -0600

    Updates

---
+++ precache-manifest.ef9a0d4064f4f8d20d23eff4eb6d6660.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "886ce26eba9f0f7a8b39439b75ed82d7",
+    "url": "/peliculas.react/index.html"
+  },
+  {
+    "revision": "2f2a171ad62621dcbf8f",
+    "url": "/peliculas.react/static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "441b700d6bfaf95b72f0",
+    "url": "/peliculas.react/static/css/main.499ea8f6.chunk.css"
+  },
+  {
+    "revision": "2f2a171ad62621dcbf8f",
+    "url": "/peliculas.react/static/js/2.51dc26c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/peliculas.react/static/js/2.51dc26c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "441b700d6bfaf95b72f0",
+    "url": "/peliculas.react/static/js/main.58c3b74f.chunk.js"
+  },
+  {
+    "revision": "afda246ef0288b81c518",
+    "url": "/peliculas.react/static/js/runtime-main.9455f3a2.js"
+  }
+]);
